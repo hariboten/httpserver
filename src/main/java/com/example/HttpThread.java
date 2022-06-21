3f5f79f7b8811c15ebea70a229dc94dc6340dab8
eisaku <e_watanabe@dreamarts.co.jp>
2022-06-21 09:56:28 +0900

    Define abstract http thread process

--- src/main/java/com/example/HttpThread.java
+++ src/main/java/com/example/HttpThread.java
@@ -1,25 +1,34 @@
+package com.example;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 
 public abstract class HttpThread implements Runnable{
 	private final InputStream in;
+	private final OutputStream out;
 	private final String docRoot;
 	
-	public HttpThread(InputStream in, String docRoot) {
+	public HttpThread(final InputStream in, final OutputStream out, final String docRoot) {
 		this.in = in;
+		this.out = out;
 		this.docRoot = docRoot;
 	}
 
 	public void run() {
-		while (true) {
-			var request = recieveRequest(in);
-			var file = openFile(request, docRoot);
-			sendResponse(request, file);
+		try {
+			while (true) {
+				var request = recieveRequest(in);
+				var file = openFile(request, docRoot);
+				sendResponse(request, file, out);
+			}
+		} catch (final IOException e) {
+			handleIOException(e);
 		}
 	}
 
-	abstract HttpRequest recieveRequest(InputStream in) throws IOException;
-	abstract InputStream openFile(HttpRequest request, String docRoot) throws IOException;
-	abstract void sendResponse(HttpRequest request, InputStream file) throws IOException;
+	abstract HttpRequest recieveRequest(final InputStream in) throws IOException;
+	abstract InputStream openFile(final HttpRequest request, final String docRoot) throws IOException;
+	abstract void sendResponse(final HttpRequest request, final InputStream file, final OutputStream out) throws IOException;
+	abstract void handleIOException(final IOException e);
 }
