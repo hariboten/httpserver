cb1a36538db75234dd730fd88ace65fd9aeb2662
eisaku <e_watanabe@dreamarts.co.jp>
2022-07-24 11:03:02 +0900

    first commit with renew project

---
+++ src/main/java/hariboten/App.java
@@ -0,0 +1,13 @@
+package hariboten;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
